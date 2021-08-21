31d5978f532f94ab1b56ddce7fc005a7953358d5
Alexey Ryabov <6006l1k@gmail.com>
2021-08-21 22:06:05 +0300

    Update api and docs

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from datetime import datetime, timedelta
 from sanic import Sanic
 from sanic import response
 import aiosqlite
@@ -16,10 +17,20 @@ app.register_middleware(db_middleware, "db")
 
 @app.route("/api/users/<user_id:int>/stat")
 async def users_stat(request, user_id: int):
+    from_date = request.args.get("from")
+    if from_date:
+        try:
+            from_date = datetime.strptime(from_date, "%Y-%m-%d")
+        except ValueError as e:
+            return response.json({"error": str(e)}, status=400)
+    else:
+        from_date = datetime.now() - timedelta(days=53 * 7)
+
     db = app.ctx.db
     cursor = await db.execute(
-        "SELECT user_id, created, added FROM stat WHERE user_id = ?",
-        (user_id, )
+        "SELECT user_id, created, added FROM stat "
+        "WHERE user_id = ? AND created >= ?",
+        (user_id, from_date.strftime("%Y-%m-%d"))
     )
     rows = await cursor.fetchall()
     await cursor.close()
